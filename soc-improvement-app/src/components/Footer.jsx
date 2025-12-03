daf6d73d1be37b1b2671e083752a6306aab2db0f
Mike <67545678+mikecybersec@users.noreply.github.com>
2025-12-03 17:29:06 +0000

    Add footer attribution with divider

---
+++ soc-improvement-app/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="app-footer">
+    Created by{' '}
+    <a href="https://github.com/mikecybersec/" target="_blank" rel="noreferrer">
+      @mikecybersec
+    </a>{' '}
+    using Codex, the source code is available on GitHub.
+  </footer>
+);
+
+export default Footer;
