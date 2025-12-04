60c1c68bf9d8c2c0ca4b232f3152dc167d2070d1
Mike <67545678+mikecybersec@users.noreply.github.com>
2025-12-04 17:14:24 +0000

    Add repository link to footer GitHub text

--- soc-improvement-app/src/components/Footer.jsx
+++ soc-improvement-app/src/components/Footer.jsx
@@ -6,7 +6,15 @@ const Footer = () => (
     <a href="https://github.com/mikecybersec/" target="_blank" rel="noreferrer">
       @mikecybersec
     </a>{' '}
-    using Codex, the source code is available on GitHub.
+    using Codex, the source code is available on{' '}
+    <a
+      href="https://github.com/mikecybersec/SOC-Compass/"
+      target="_blank"
+      rel="noreferrer"
+    >
+      GitHub
+    </a>
+    .
   </footer>
 );
 
