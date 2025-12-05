cfefb1490279f0cc1e5b890f3805015aabbc9022
Mike <67545678+mikecybersec@users.noreply.github.com>
2025-12-05 08:30:51 +0000

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,9 @@
+# -- Project Information -----------------------------------------------------
+project = "SOC-Compass"
+author = "mikecybersec"
+
+# -- General Configuration ---------------------------------------------------
+extensions = []
+
+# -- Options for HTML Output -------------------------------------------------
+html_theme = "sphinx_rtd_theme"
